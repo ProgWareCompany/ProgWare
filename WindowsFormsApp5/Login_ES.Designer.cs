35d500e2f3e83314521b9cc24336af7be3890890
alumno <alumno@alumno>
2022-06-20 16:48:16 -0300

    inicio

---
+++ WindowsFormsApp5/Login_ES.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Login_ES
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtcontra = new System.Windows.Forms.TextBox();
+            this.Ingresar = new System.Windows.Forms.Button();
+            this.btnBusqueda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.SlateGray;
+            this.label1.Location = new System.Drawing.Point(533, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Program by ProgWare";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 76);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "S.I.G.D";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario (C.I.): ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contraseña:";
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(271, 158);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(131, 20);
+            this.txtusuario.TabIndex = 4;
+            this.txtusuario.TextChanged += new System.EventHandler(this.txtusuario_TextChanged);
+            // 
+            // txtcontra
+            // 
+            this.txtcontra.Location = new System.Drawing.Point(271, 186);
+            this.txtcontra.Name = "txtcontra";
+            this.txtcontra.Size = new System.Drawing.Size(131, 20);
+            this.txtcontra.TabIndex = 5;
+            this.txtcontra.TextChanged += new System.EventHandler(this.txtcontra_TextChanged);
+            // 
+            // Ingresar
+            // 
+            this.Ingresar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Ingresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ingresar.Location = new System.Drawing.Point(271, 212);
+            this.Ingresar.Name = "Ingresar";
+            this.Ingresar.Size = new System.Drawing.Size(105, 23);
+            this.Ingresar.TabIndex = 9;
+            this.Ingresar.Text = "ingresar";
+            this.Ingresar.UseVisualStyleBackColor = true;
+            this.Ingresar.Click += new System.EventHandler(this.ingresar_Click);
+            // 
+            // btnBusqueda
+            // 
+            this.btnBusqueda.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnBusqueda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBusqueda.Location = new System.Drawing.Point(271, 241);
+            this.btnBusqueda.Name = "btnBusqueda";
+            this.btnBusqueda.Size = new System.Drawing.Size(105, 23);
+            this.btnBusqueda.TabIndex = 10;
+            this.btnBusqueda.Text = "Buscar Jugador";
+            this.btnBusqueda.UseVisualStyleBackColor = true;
+            this.btnBusqueda.Click += new System.EventHandler(this.invitado_Click);
+            // 
+            // Login_ES
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 323);
+            this.Controls.Add(this.btnBusqueda);
+            this.Controls.Add(this.Ingresar);
+            this.Controls.Add(this.txtcontra);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login_ES";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_ES_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtcontra;
+        private System.Windows.Forms.Button Ingresar;
+        private System.Windows.Forms.Button btnBusqueda;
+    }
+}
+
---
+++ WindowsFormsApp5/Login_ES.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Login_ES : Form
+    {
+        public Login_ES()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void txtusuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcontra_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void ingresar_Click(object sender, EventArgs e)
+        {
+
+
+
+           
+        }
+
+        
+
+        private void invitado_Click(object sender, EventArgs e)
+        {
+            invitado_ES Ventana = new invitado_ES();
+            ActiveForm.Hide();
+            Ventana.Show();
+
+            this.Hide();
+        }
+
+        private void Login_ES_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
